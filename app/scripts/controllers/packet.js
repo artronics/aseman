dee1a2040caee03d27c8682bb3e3049d23b707f6
Seyed Jalal Hosseini <jalalhosseiny@gmail.com>
2015-12-03 18:45:54 +0100

    update

--- app/scripts/controllers/packet.js
+++ app/scripts/controllers/packet.js
@@ -6,5 +6,7 @@ angular.module('asemanApp')
 
         //var packetsObj = Restangular.all('packets').getList;
         //self.packets = packetsObj.packets;
+        //self.packets = {kir:'los',kos:[{kd:'dool'},{kj:'st'}]}
         self.packets = Restangular.all('packets').getList();
+        console.log(self.packets);
     }]);
